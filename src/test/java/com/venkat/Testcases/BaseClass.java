ae0ebd5bfd670c504398268d436d0605c2e8773a
venkat <venkey402@gmail.com>
2024-06-20 11:10:50 +0530

    modified base to add implicit and pageload timeouts

--- src/test/java/com/venkat/Testcases/BaseClass.java
+++ src/test/java/com/venkat/Testcases/BaseClass.java
@@ -1,5 +1,8 @@
 package com.venkat.Testcases;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
@@ -50,9 +53,13 @@ public class BaseClass {
 		}
 		
 		driver.manage().window().maximize();
-		logger.info("Browser window maximised");
+		logger.info("Browser window maximized.");
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		logger.info("Implicit time out is set.");
+		driver.manage().timeouts().pageLoadTimeout(10,TimeUnit.SECONDS);
+		logger.info("Page load time out is set.");
 		driver.get(baseUrl);
-		logger.info("Opened url "+ baseUrl);
+		logger.info("Opened url "+ baseUrl+"  successfully.");
 	}
 	
 	@AfterTest
